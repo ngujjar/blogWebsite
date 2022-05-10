c0a68f5ee7b49e967ce197570797882d5b3ae9ed
Naval Kishor <naval1901260@gmail.com>
2022-05-10 23:36:17 +0530

    second commit

--- connections/config.js
+++ connections/config.js
@@ -8,14 +8,13 @@ const user = process.env.USER;
 const clusterURL = process.env.URL;
 
 // For Localhost Database...
-const uri = "mongodb://localhost:27017/blogWebsite";
+// const uri = "mongodb://localhost:27017/blogWebsite";
 
 // For MongoDB Atlas Database...
-// const uri = "mongodb+srv://" + user + ":" + password + "@" + clusterURL + db + "?retryWrites=true&w=majority";
+const uri = "mongodb+srv://" + user + ":" + password + "@" + clusterURL + db + "?retryWrites=true&w=majority";
 
 // Connecting to the database...
 mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
-
 
 const dbConnection = mongoose.connection;
 module.exports = dbConnection;
